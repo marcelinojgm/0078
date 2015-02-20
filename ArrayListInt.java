ef5af0cbc98371170442bb92a04420472b0d8d85
MJGM <MJGM@example.com>
2015-02-20 12:13:48 +0100

    realizado constructor de la clase ArrayListInt

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -8,26 +8,17 @@
 public class ArrayListInt
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int[] list;
 
     /**
      * Constructor for objects of class ArrayListInt
      */
     public ArrayListInt()
     {
-        // initialise instance variables
-        x = 0;
+        list = new int[0];
     }
+    
+   
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
