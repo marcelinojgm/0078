32d998f0049ef1a6c6b73c3ef75f8053e1b17115
MJGM <MJGM@example.com>
2015-02-20 12:36:02 +0100

    añadidos metodos que añaden elementos a la lista add(int num) que añade un elemento nuevo y add(int index, int num)que modifica el valor del indice indicado por el valor indicado

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -17,8 +17,38 @@ public class ArrayListInt
     {
         list = new int[0];
     }
-    
-   
 
-    
+    /**
+     * añade otro elemento a la lista
+     */
+
+    public void add(int num)
+    {
+        //nueva lista creada
+        int[] newList = new int[list.length + 1];
+        //copiado de lista antigua en la nueva
+        for (int index = 0; index < list.length; index++ )
+        {
+            newList[index] = list[index];
+        }
+        //agregado de nuevo elemento en la nueva lista
+        newList[list.length] = num;
+
+        //guardado de la nueva lista
+        list = newList;
+    }
+
+    /**
+     * introduce un int num en la posicion indicada index
+     */
+    public void add(int index, int num)
+    {
+         //nueva lista creada
+         if(index < list.length)
+         {
+             list[index] = num;
+         }
+    }
+
+
 }
